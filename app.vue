892f4aaee5da7cfebc4c960ae4e54a88b7d5bb17
Akayy <techakayy@gmail.com>
2024-09-14 19:44:11 +1000

    Added pages

--- app.vue
+++ app.vue
@@ -1,10 +1,5 @@
 <script setup lang="ts">
   const route = useRoute()
-  const count = ref(0)
-  const {data} = await useFetch('/api/data')
-  prerenderRoutes('/api/data')
-
-  const {products, date} = data.value
 
   if (route.path === '/no-js') {
     useServerHead({
@@ -24,24 +19,8 @@
 
 <template>
   <div>
-    <h1>
-      Route: {{ route.path }}
-      <br />
-      Server date: {{ date }}
-      <br />
-      Product Count: {{ products.length }}
-    </h1>
-
-    <button class="bg-gray-200 p-2 rounded-lg" id="count" @click="count++">
-      Count: {{ count }}
-    </button>
-
-    <ClientOnly>
-      <h2>Client Only Info</h2>
-      <p>This is not rendered on the server.</p>
-      <p>Client date: {{ new Date().toISOString() }}</p>
-    </ClientOnly>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
-
-<style scoped></style>
